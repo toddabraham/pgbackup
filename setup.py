ee98d0b13a98bcd277182ca8066d6fe71b9750fa
Cloud User <centos@ip-172-31-17-51.ap-southeast-1.compute.internal>
2022-07-06 14:34:14 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+with open('README.md', 'r') as f:
+ long_description = f.read()
+setup(
+ name='pgpackup',
+ version='0.1.0',
+ author='Todd Abraham',
+ author_email='todd.apis@gmail.com',
+ description='A utility for backing up PostgreSQL databases',
+ long_description=long_description,
+ long_description_content_type='text/markdown',
+ url='https://github.com/toddabraham/pgbackup.git',
+ packages=find_packages('src')
+)
