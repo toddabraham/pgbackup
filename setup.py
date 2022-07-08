2b92eba892975b9cd1210de623660b7b54e05e1c
Cloud User <centos@ip-172-31-17-51.ap-southeast-1.compute.internal>
2022-07-08 03:39:16 +0000

    Final updates to setup, cli, and pgdump

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 from setuptools import find_packages, setup
+
 with open('README.md', 'r') as f:
- long_description = f.read()
+    long_description = f.read()
 setup(
- name='pgpackup',
- version='0.1.0',
- author='Todd Abraham',
- author_email='todd.apis@gmail.com',
- description='A utility for backing up PostgreSQL databases',
- long_description=long_description,
- long_description_content_type='text/markdown',
- url='https://github.com/toddabraham/pgbackup.git',
- packages=find_packages('src')
+    name='pgpackup',
+    version='0.1.0',
+    author='Todd Abraham',
+    author_email='todd.apis@gmail.com',
+    description='A utility for backing up PostgreSQL databases.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/toddabraham/pgbackup.git',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['boto3'],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'pgbackup=pgbackup.cli:main',
+        ],
+    }
 )
